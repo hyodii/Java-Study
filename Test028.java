a5745344f7f1d2d26d3bef4289876abba591e46d
HyoJin Jeong <87711421+hyodii@users.noreply.github.com>
2021-07-25 17:00:53 +0900

    Update Test028.java

--- Test028.java
+++ Test028.java
@@ -29,7 +29,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class hyodii028
+public class Test028
 {
 	public static void main(String[] args) throws IOException
 	{
