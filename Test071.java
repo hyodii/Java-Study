03509db59c8b2f33244fd9adda7f51276d7facce
HyoJin Jeong <87711421+hyodii@users.noreply.github.com>
2021-07-29 00:02:52 +0900

    update this

--- Test071.java
+++ Test071.java
@@ -7,14 +7,14 @@ public class Test071
 	{
 		this(1000);
 		x=10;
-		System.out.println("인자가 없는 생성자")
+		System.out.println("인자가 없는 생성자");
 		System.out.println("Test071이 갖고있는 x : "+x);
 	}
 	Test071(int x)
 	{
 		this.x = x;
 		System.out.println("인자가 하나인 생성자");
-		system.out.println("Test071이 갖고있는 x : "+x);
+		System.out.println("Test071이 갖고있는 x : "+x);
 	}
 
 	public static void main(String[] args)
