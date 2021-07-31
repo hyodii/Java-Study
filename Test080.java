5c8e63159d55ee8426364b4aa1aec70603045cee
HyoJin Jeong <87711421+hyodii@users.noreply.github.com>
2021-07-31 19:30:01 +0900

    2d array
    2차원 배열 사용하기

---
+++ Test080.java
@@ -0,0 +1,56 @@
+public class Test080
+{
+	public static void main(String[] args)
+	{
+		int[][] arr1 = new int[3][3];
+		arr1[0][0] = 1;
+		arr1[0][1] = 2;
+		arr1[0][2] = 3;
+		arr1[1][0] = 4;
+		arr1[1][1] = 5;
+		arr1[1][2] = 6;
+		arr1[2][0] = 7;
+		arr1[2][1] = 8;
+		arr1[2][2] = 9;
+
+		for (int i=0 ; i<arr1.length; i++)
+			for (int j=0; j<arr1[i].length; j++)
+			{
+				System.out.print(arr1[i][j]+ " ");
+			}System.out.println();
+			//--==>> 1 2 3 4 5 6 7 8 9
+
+		int[][] arr2 = {{1,2,3},{4,5,6},{7,8,9}};
+		for(int i=0;i<arr2.length;i++)
+			for (int j=0; j<arr2[i].length; j++)
+			{
+				System.out.print(arr2[i][j]+" ");
+			}System.out.println();
+			//--==>> 1 2 3 4 5 6 7 8 9
+		
+		int[][] arr3 = new int[3][3];
+		int n = 1;
+		for (int i=0; i<arr3.length; i++)
+			for (int j=0; j<arr3[i].length;j++ )
+			{
+				arr3[i][j] = n;
+				n++;
+			}
+
+		for (int i=0; i<arr3.length;i++ )
+		{
+			for (int j=0; j<arr3[i].length; j++)
+			{
+				System.out.printf("%2d",arr3[i][j]);
+			}
+		}System.out.println();
+		//--==>> 1 2 3 4 5 6 7 8 9
+			
+	}
+}
+/*
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+ 1 2 3 4 5 6 7 8 9
+계속하려면 아무 키나 누르십시오 . . .
+*/
