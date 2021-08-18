55072188df148d433a76303b9eb4bbc8564af8e2
HyoJin Jeong <87711421+hyodii@users.noreply.github.com>
2021-08-19 01:02:13 +0900

    AnimalTest Overriding
    // 컴파일 타임 다형성 - 오버로딩
    // 같은 이름의 메소드를 매개 변수의 개수 또는 타입을 변경하여 여러개 사용

---
+++ AnimalTest.java
@@ -0,0 +1,57 @@
+class Animal
+{
+	public void move()
+	{
+		System.out.println("동물이 움직입니다.");
+	}
+}
+
+class Human extends Animal
+{
+	public void move()
+	{
+		System.out.println("사람이 두 발로 뜁니다.");
+	}
+}
+
+class Tiger extends Animal
+{
+	public void move()
+	{
+		System.out.println("호랑이가 네 발로 뜁니다.");
+	}
+}
+
+class Eagle extends Animal
+{
+	public void move()
+	{
+		System.out.println("독수리가 날아갑니다.");
+	}
+}
+
+public class AnimalTest
+{
+	public static void main(String[] args) throws java.lang.Exception
+	{
+		Animal human = new Human();
+		Animal tiger = new Tiger();
+		Animal eagle = new Eagle();
+
+		AnimalTest test = new AnimalTest();
+		test.moveAnimal(human);
+		test.moveAnimal(tiger);
+		test.moveAnimal(eagle);
+	}
+	
+	public void moveAnimal(Animal animal)
+	{
+		animal.move();
+	}
+}
+/*
+사람이 두 발로 뜁니다.
+호랑이가 네 발로 뜁니다.
+독수리가 날아갑니다.
+계속하려면 아무 키나 누르십시오 . . .
+*/
